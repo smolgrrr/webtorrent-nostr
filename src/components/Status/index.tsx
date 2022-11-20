521226b3ba76f8561df65e8c3f28364024b38c89
negu63 <negu6633@gmail.com>
2022-11-21 01:22:37 +0900

    feat: Add Status component

---
+++ src/components/Status/index.tsx
@@ -0,0 +1,27 @@
+import { StatusInterface } from "../../types/progress";
+
+export const Status = ({
+  statusCode,
+  status,
+}: {
+  statusCode: string;
+  status: StatusInterface | null;
+}) => {
+  return (
+    <>
+      {statusCode === "ready" ? (
+        <div>{"ready"}</div>
+      ) : statusCode === "wait" ? (
+        <div>{"find torrent..."}</div>
+      ) : (
+        <div>
+          <div>{`total: ${status?.total}`}</div>
+          <div>{`downloaded: ${status?.downloaded}`}</div>
+          <div>{`download: ↓ ${status?.downloadSpeed}`}</div>
+          <div>{`upload: ↑ ${status?.uploadSpeed}`}</div>
+          <div>{`progress: ${status?.progress}%`}</div>
+        </div>
+      )}
+    </>
+  );
+};
